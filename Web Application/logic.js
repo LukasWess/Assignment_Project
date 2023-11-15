9a3e3d05aa55fb4e8b2d3a63ed5de69a9380aaea
Lukas Wessels <Lukas25@live.no>
2023-11-15 16:25:33 +0100

    Staff table completion, modal toast implementation

--- Web Application/logic.js
+++ Web Application/logic.js
@@ -1,12 +1,30 @@
-function updateClock() {
-    var now = new Date();
-    var hours = now.getHours();
-    var minutes = now.getMinutes();
-    var seconds = now.getSeconds();
-    document.getElementById('clock').innerHTML = hours + ':' + minutes + ':' + seconds;
+// Define the digitalClock function
+function digitalClock() {
+    var date = new Date();
+    var day = date.getDate();
+    var month = date.getMonth() + 1; // months are zero-based
+    var year = date.getFullYear();
+    var hours = date.getHours();
+    var minutes = date.getMinutes();
+    var seconds = date.getSeconds();
+
+    // pad single-digit day, month, hours, minutes, and seconds with a leading zero
+    day = (day < 10) ? '0' + day : day;
+    month = (month < 10) ? '0' + month : month;
+    hours = (hours < 10) ? '0' + hours : hours;
+    minutes = (minutes < 10) ? '0' + minutes : minutes;
+    seconds = (seconds < 10) ? '0' + seconds : seconds;
+
+    var formattedTime = day + '-' + month + '-' + year + ' ' + hours + ':' + minutes + ':' + seconds;
+
+    // ppdate the clock div
+    document.getElementById('clock').innerHTML = formattedTime;
 }
 
-setInterval(updateClock, 1000);
+// Call digitalClock every second
+setInterval(digitalClock, 1000);
+
+// Rest of your code...
 
 // Define the Employee class
 class Employee {
@@ -22,7 +40,7 @@ class StaffMember extends Employee {
         super(name, surname);
         this.picture = picture;
         this.email = email;
-        this.status = status;
+        this.status = 'In';
         this.outTime = outTime;
         this.duration = duration;
         this.expectedReturnTime = expectedReturnTime;
@@ -44,7 +62,9 @@ class DeliveryDriver extends Employee {
 }
 
 var selectedStaffMember = null;
-
+//StaffUserGet Function doing a ajax call to the randomuser api and getting 5 random users and displaying them in a table
+//The function is creating objects and dynamicly populating the html table, changing the nr of result will change the nr of users displayed. 
+//this makes this code very scalable and easy to use.
 $(document).ready(function() {
     $.ajax({
         url: 'https://randomuser.me/api/?results=5',
@@ -54,7 +74,7 @@ $(document).ready(function() {
             $.each(data.results, function(i, item) {
                 var staffMember = new StaffMember(item.name.first, item.name.last, item.picture.thumbnail, item.email);
                 var tr = $('<tr>').append(
-                    $('<td>').html('<img src="' + staffMember.picture + '" alt="Profile Picture">'),
+                    $('<td>').html('<img src="' + staffMember.picture + '" alt="Profile Picture" class="rounded-image">'),
                     $('<td>').text(staffMember.name),
                     $('<td>').text(staffMember.surname),
                     $('<td>').text(staffMember.email),
@@ -63,6 +83,7 @@ $(document).ready(function() {
                     $('<td>').text(staffMember.duration), // duration
                     $('<td>').text(staffMember.expectedReturnTime), // expectedReturnTime
                     $('<td>').text(staffMember.staffMemberIsLate), // staffMemberIsLate
+                    
                 ).data('staffMember', staffMember);
                 staffTable.append(tr);
             });
@@ -80,14 +101,64 @@ $(document).ready(function() {
         }
     });
 
-    $('#in-button').on('click', function() {
-        if (selectedStaffMember) {
-            selectedStaffMember.status = 'In';
-            $('#staffTable tr.selected').children().eq(4).text('In');
+    
+});
+
+$('#out-button').on('click', function() {
+    if (selectedStaffMember) {
+        $('#modalTitle').text('Enter out-time for ' + selectedStaffMember.name);
+        $('#durationModal').modal('show');
+    }
+});
+
+$(document).ready(function() {
+    $('#confirmButton').on('click', function() {
+        console.log('Confirm button clicked');
+        var duration = $('#durationInput').val();
+        duration = parseInt(duration);
+
+        if (!isNaN(duration)) {
+            var outTime = new Date();
+            var expectedReturnTime = new Date(outTime.getTime() + duration * 60000);
+
+            var clockTime = document.getElementById('clock').innerHTML.split(' ')[1]; // Get the time from the digital clock
+            var hoursAndMinutes = clockTime.split(':').slice(0, 2).join(':'); // Get only the hours and minutes
+
+            selectedStaffMember.status = 'Out';
+            selectedStaffMember.outTime = hoursAndMinutes;
+            selectedStaffMember.duration = Math.floor(duration / 60) + 'h ' + (duration % 60) + 'min';
+            selectedStaffMember.expectedReturnTime = expectedReturnTime.getHours() + ':' + expectedReturnTime.getMinutes();
+
+            var selectedRow = $('#staffTable tr.selected');
+            selectedRow.children().eq(4).text('Out');
+            selectedRow.children().eq(5).text(selectedStaffMember.outTime);
+            selectedRow.children().eq(6).text(selectedStaffMember.duration);
+            selectedRow.children().eq(7).text(selectedStaffMember.expectedReturnTime);
 
             // Unselect the row
-            $('#staffTable tr.selected').removeClass('selected');
+            selectedRow.removeClass('selected');
             selectedStaffMember = null;
         }
+
+        $('#durationModal').modal('hide');
     });
+});
+
+$('#in-button').on('click', function() {
+    if (selectedStaffMember) {
+        selectedStaffMember.status = 'In';
+        selectedStaffMember.outTime = '';
+        selectedStaffMember.duration = '';
+        selectedStaffMember.expectedReturnTime = '';
+
+        var selectedRow = $('#staffTable tr.selected');
+        selectedRow.children().eq(4).text('In');
+        selectedRow.children().eq(5).text('');
+        selectedRow.children().eq(6).text('');
+        selectedRow.children().eq(7).text('');
+
+        // Unselect the row
+        selectedRow.removeClass('selected');
+        selectedStaffMember = null;
+    }
 });
